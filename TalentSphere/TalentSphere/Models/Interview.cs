ad4776ebaee47b681bbc6bbdb84a9bede5adcbdf
Yadav <2478616@cognizant.com>
2026-03-10 12:25:58 +0530

    Added Employee and User Model

--- TalentSphere/TalentSphere/Models/Interview.cs
+++ TalentSphere/TalentSphere/Models/Interview.cs
@@ -2,7 +2,6 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using TalentSphere.Models;
-using TalentSphere.Models.User;
 
 namespace TalentSphereAPI.Models
 {
