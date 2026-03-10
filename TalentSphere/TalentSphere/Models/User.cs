d2d674e88892272d1c49ba6cda3fcf9a8709a573
Yadav <2478616@cognizant.com>
2026-03-10 12:01:48 +0530

    Added user model

--- TalentSphere/TalentSphere/Models/User.cs
+++ TalentSphere/TalentSphere/Models/User.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace TalentSphere.Models.User
+namespace TalentSphere.Models
 {
     public class User
     {
